a477ec40231f160a0d2b74d7d301daee2c7d8f35
lwaghabi <130769699+lwaghabi@users.noreply.github.com>
2026-04-14 16:56:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -323,10 +323,10 @@ def test_db():
         import mysql.connector
         import os
         conn = mysql.connector.connect(
-            host=os.getenv('mysql.sistemaos.com.br'),
-            user=os.getenv('sistemaos03'),
-            password=os.getenv('zinholui47'),
-            database=os.getenv('sistemaos03'),
+            host=os.getenv('DB_HOST','mysql.sistemaos.com.br'),
+            user=os.getenv('DB_USER','sistemaos03'),
+            password=os.getenv('DB_PASSWORD','zinholui47'),
+            database=os.getenv('DB_NAME','sistemaos03'),
             port=int(os.getenv('DB_PORT', 3306))
         )
         conn.close()
